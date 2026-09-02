e977b1c77467b2349e422b38d0a4b6361b09797b
Thunder-CodeEditer <thunsawangsri@gmail.com>
2026-09-03 03:15:54 +0700

    fix victory screen

--- scripts/win_scene.gd
+++ scripts/win_scene.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
 @onready var audio: AudioStreamPlayer = $AudioStreamPlayer
-@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var victory_image: TextureRect = $VictoryImage
 @onready var continue_button: Button = $ContinueButton
+
+const FADE_TIME: float = 0.6
 
 func _ready() -> void:
 	audio.play()
-	anim.play("win_intro")
 	continue_button.pressed.connect(_on_continue_pressed)
+
+	victory_image.modulate.a = 0.0
+	var tween: Tween = create_tween()
+	tween.tween_property(victory_image, "modulate:a", 1.0, FADE_TIME)
 
 func _on_continue_pressed() -> void:
 	Transition.fade_to_scene("res://scenes/menu.tscn")
